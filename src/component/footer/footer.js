0545fa62b094929ee3dc725a9eeaddb2332bd498
Jericho Irvin <aldejerichoirvin@gmail.com>
2020-12-10 07:25:04 +0800

    initial git commit

---
+++ src/component/footer/footer.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+const Footer = () => (
+  <div>
+    <span>This is the footer.</span>
+  </div>
+)
+
+export default Footer
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,10 @@
 import React from "react"
+import Layout from "../component/layout"
 
-export default function Home() {
-  return <div>Hello world!</div>
-}
+const Home = () => (
+  <Layout>
+    <div>Hello world!</div>
+  </Layout>
+)
+
+export default Home
